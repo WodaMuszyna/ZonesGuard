9dfdc6d104f880da32042d5eeacb489823ce138f
WodaMuszyna <j878025@gmail.com>
2021-08-08 18:15:18 +0200

    Add zone creation and flag modification

--- src/main/java/me/wodamuszyna/zones/commands/ZonesCommand.java
+++ src/main/java/me/wodamuszyna/zones/commands/ZonesCommand.java
@@ -9,10 +9,13 @@ import me.wodamuszyna.zones.Main;
 import me.wodamuszyna.zones.data.StateFlag;
 import me.wodamuszyna.zones.data.Zone;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.Map;
 
 public class ZonesCommand implements CommandExecutor {
     @Override
@@ -30,9 +33,14 @@ public class ZonesCommand implements CommandExecutor {
                     s.sendMessage("Error! Select the region before defining.");
                     return false;
                 }
-                //zone creation code
+                Location min = new Location(BukkitAdapter.adapt(region.getWorld()), region.getMinimumPoint().getBlockX(), region.getMinimumPoint().getBlockY(), region.getMinimumPoint().getZ());
+                Location max = new Location(BukkitAdapter.adapt(region.getWorld()), region.getMaximumPoint().getBlockX(), region.getMaximumPoint().getBlockY(), region.getMaximumPoint().getZ());
+                Zone z = new Zone(a[1], min, max);
+                s.sendMessage(String.format("Zone %s has been created", z.getName()));
+                return true;
             } catch (IncompleteRegionException e) {
-                e.printStackTrace();
+                s.sendMessage("Error! Select the region before defining.");
+                return false;
             }
         }
         if(a[0].equalsIgnoreCase("flag")){
@@ -51,6 +59,24 @@ public class ZonesCommand implements CommandExecutor {
                 return false;
             }
             Main.getManager().updateFlag(z, flag, Main.getManager().parseInput(a[3]));
+            z.markDirty();
+            s.sendMessage(String.format("Value of flag %s has been set to %s", flag.getName(), Main.getManager().parseInput(a[3]).name()));
+        }
+        if(a[0].equalsIgnoreCase("flags")){
+            if(a.length < 2){
+                s.sendMessage("Correct usage: /zg flags <name>");
+                return false;
+            }
+            Zone z = Main.getManager().getZone(a[1]);
+            if(z == null){
+                s.sendMessage(String.format("Zone %s does not exist", a[1]));
+                return false;
+            }
+            StringBuilder flags = new StringBuilder();
+            for(Map.Entry<StateFlag, StateFlag.State> entry : z.getFlags().entrySet()){
+                flags.append(entry.getKey().getName()).append(": ").append(Main.getManager().parseOutput(entry.getValue())).append(", ");
+            }
+            s.sendMessage(flags.toString());
         }
         return false;
     }
